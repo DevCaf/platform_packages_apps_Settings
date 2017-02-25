66989e1f1623c8d0d289e86d1712747d51258bec
spezi77 <spezi77@gmx.com>
2017-02-25 13:11:31 +0100

    Settings: NebulaSettings initial checkin.
    Change-Id: Iab7c11d3bba14bd7c9a6b354af6fda63f7228ce0
    Signed-off-by: spezi77 <spezi77@gmx.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -144,6 +144,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import com.nebula.settings.NebulaSettings;
+
 public class SettingsActivity extends SettingsDrawerActivity
         implements PreferenceManager.OnPreferenceTreeClickListener,
         PreferenceFragment.OnPreferenceStartFragmentCallback,
@@ -388,7 +390,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            ButtonsSettings.class.getName()
+            ButtonsSettings.class.getName(),
+            NebulaSettings.class.getName()
     };
 
 
@@ -594,6 +597,7 @@ public class SettingsActivity extends SettingsDrawerActivity
 
         mIsShowingDashboard = className.equals(Settings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName())
+                || className.equals(Settings.NebulaSettings.class.getName())
                 || className.equals(Settings.DeviceSettings.class.getName())
                 || className.equals(Settings.PersonalSettings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName());
